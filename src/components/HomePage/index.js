b3d4feb23b9ff706e0347950643acbb46efa00bb
madstone0-0 <mhquansah@gmail.com>
2019-05-28 21:02:55 +0000

    Added WIP homepage

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => (
+    <div>
+        <h1 className="homepage-wip">
+            Yup this the homepage <sub>(WIP)</sub>
+        </h1>
+    </div>
+);
+
+export default HomePage;
