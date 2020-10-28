1830c7aa8a4e78ec9cd0a793e6bcb9bed709a909
madstone0-0 <mhquansah@gmail.com>
2020-10-28 19:27:59 +0000

    Improved footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="text-center card-footer">
+        <div className="centered footer">
+            <hr />
             <footer>
                 <h4>
-                    Created By{" "}
-                    <a href="https://github.com/madstone0-0">madstone0-0</a>
+                    <footer>
+                        Created By{" "}
+                        <a href="https://github.com/madstone0-0">madstone0-0</a>
+                    </footer>
                 </h4>
             </footer>
         </div>
