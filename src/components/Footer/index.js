ec0b2845c3a1dfff99fc8b37a1cbb3a5b6bf38c3
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-08-22 19:44:30 +0000

    Added proper attribution

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,6 +12,11 @@ const Footer = () => {
                     <footer>
                         Created By{" "}
                         <a href="https://github.com/madstone0-0">madstone0-0</a>
+                        <p>
+                            <a href="http://marvel.com">
+                                Data provided by Marvel. © 2022 MARVEL
+                            </a>
+                        </p>
                     </footer>
                 </h4>
             </footer>
