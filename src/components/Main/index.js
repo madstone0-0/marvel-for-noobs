351d6412aa7ca8a17400d6a9e70a01ddc3417944
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-08-22 21:23:19 +0000

    Rewrote the Main component as a function

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-console */
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Menu } from "semantic-ui-react";
 import { Route, Link, Routes } from "react-router-dom";
 import axios from "axios";
@@ -17,191 +16,198 @@ import {
 import CharacterGrid from "../CharacterGrid";
 import HomePage from "../HomePage";
 import Footer from "../Footer";
+import { element } from "prop-types";
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            darkTheme: false,
-            searchCharacter: "",
-            characters: [],
-            error: null,
-            isLoading: false,
-            hasNotSearchedOnce: true,
-        };
-    }
+const Main = () => {
+    const [darkTheme, updateDarkTheme] = useState(false);
+    const [searchCharacter, updateSearchCharacter] = useState("");
+    const [characters, updateCharacterArray] = useState([]);
+    const [error, updateErrorState] = useState(null);
+    const [isLoading, updateLoadingState] = useState(false);
+    const [hasNotSearchedOnce, hasSearchedOnce] = useState(true);
 
-    onSearchChange = (event) => {
-        return this.setState({ searchCharacter: event.target.value });
+    const cookies = new Cookies();
+    const searchCharacterCache = new Map();
+
+    useEffect(() => {
+        ReactGA.initialize("UA-131448417-2");
+        ReactGA.pageview(window.location.pathname + window.location.search);
+        getDarkModeCookie();
+        // getStoredCharacters();
+        // if (performance.navigation.type === 1) {
+        //     window.location.href = "/";
+        // }
+    }, []);
+
+    useEffect(() => {
+        setDarkModeCookie();
+    }, [darkTheme]);
+
+    // useEffect(() => {
+    //     searchCharacterCache.set(searchCharacter, characters);
+    //     window.localStorage.setItem(
+    //         searchCharacter.toLocaleLowerCase(),
+    //         JSON.stringify(searchCharacterCache.get(searchCharacter)),
+    //     );
+    // }, [characters]);
+
+    // const getStoredCharacters = () => {
+    //     const keys = Object.keys(localStorage);
+    //     keys.forEach((key) => {
+    //         searchCharacterCache.set(key, Object(localStorage.getItem(key)));
+    //     });
+    //     console.log(searchCharacterCache);
+    // };
+
+    const onSearchChange = (e) => {
+        return updateSearchCharacter(e.target.value);
     };
 
-    onSearchSubmit = (event) => {
+    const onSearchSubmit = (e) => {
         ReactGA.event({
             category: "Page interactions",
             action: "Searched for a character",
         });
-        const { searchCharacter } = this.state;
-        this.fetchSearchedCharacter(searchCharacter);
-        event.preventDefault();
+        fetchSearchedCharacter(searchCharacter);
+        // console.log(searchCharacterCache);
+        // if (!searchCharacterCache.has(searchCharacter.toLocaleLowerCase())) {
+        //     fetchSearchedCharacter(searchCharacter);
+        //     searchCharacterCache.set(
+        //         searchCharacter.toLocaleLowerCase(),
+        //         characters,
+        //     );
+        //     console.log(searchCharacterCache);
+        //     console.log(searchCharacterCache.size);
+        // }
+        // updateCharacterArray(
+        //     searchCharacterCache.get(searchCharacter.toLocaleLowerCase()),
+        // );
+        e.preventDefault();
     };
 
-    setSearchedCharacter = (result) => {
-        const results = result.data.results;
-        this.setState({ characters: results, isLoading: false });
-    };
-
-    fetchSearchedCharacter = (searchCharacter) => {
-        this.setState({ isLoading: true, hasNotSearchedOnce: false });
+    const fetchSearchedCharacter = (searchCharacter) => {
+        updateLoadingState(true);
+        hasSearchedOnce(false);
         axios
             .get(
                 `${PATH_BASE}${CHARACTERS}?${PATH_SEARCH_STARTS}=${searchCharacter}&${API_KEY}&limit=${LIMIT}`,
             )
             .then((result) => {
-                this.setSearchedCharacter(result.data);
+                updateCharacterArray(result.data.data.results);
+                updateLoadingState(false);
             })
             .catch((error) => {
-                this.setState({ error });
+                updateErrorState(error);
             });
     };
 
-    setDarkModeCookie = () => {
-        const cookies = new Cookies();
-        cookies.set("darkMode", this.state.darkTheme, { path: "/" });
+    const setDarkModeCookie = () => {
+        cookies.set("darkMode", darkTheme, { path: "/" });
     };
 
-    getDarkModeCookie = () => {
-        const cookies = new Cookies();
+    const getDarkModeCookie = () => {
         const darkMode = cookies.get("darkMode");
         if (darkMode === "true") {
-            this.setState({ darkTheme: true });
+            updateDarkTheme(true);
         }
     };
 
-    toggleDarkTheme = () => {
+    const toggleDarkTheme = () => {
         ReactGA.event({
             category: "Page Interactions",
             action: "Enabled dark theme",
         });
-        this.setState({ darkTheme: !this.state.darkTheme });
+        updateDarkTheme(!darkTheme);
     };
 
-    componentDidMount = () => {
-        ReactGA.initialize("UA-131448417-2");
-        ReactGA.pageview(window.location.pathname + window.location.search);
-        this.getDarkModeCookie();
-    };
-
-    componentDidUpdate = () => {
-        this.setDarkModeCookie();
-        if (performance.navigation.type === 1) {
-            window.location.href = "/";
-        }
-    };
-
-    render() {
-        const {
-            searchCharacter,
-            characters,
-            error,
-            isLoading,
-            hasNotSearchedOnce,
-            darkTheme,
-        } = this.state;
-        return (
-            <div
-                className={classNames("", {
-                    dark: darkTheme === true,
-                })}
-            >
-                <div className="page">
-                    <Container className="page-container">
-                        <Menu className="nav-bar">
-                            <div>
-                                <button
-                                    type="button"
-                                    onClick={this.toggleDarkTheme}
-                                    className="dark-button ui red medium button search-button"
-                                >
-                                    Dark Mode
-                                </button>
-                            </div>
-                            <Container>
-                                <h1 className="page-header">
-                                    Marvel for Noobs
-                                </h1>
-                            </Container>
-                            <Menu.Item className="menu-item header-link nav-button">
-                                <Link to="/">Home</Link>
-                            </Menu.Item>
-                            <Menu.Item className="menu-item header-link nav-button">
-                                <Link to="/characters">Characters</Link>
-                            </Menu.Item>
-                        </Menu>
+    return (
+        <div
+            className={classNames("", {
+                dark: darkTheme === true,
+            })}
+        >
+            <div className="page">
+                <Container className="page-container">
+                    <Menu className="nav-bar">
                         <div>
-                            <Routes>
-                                <Route
-                                    path="characters/*"
-                                    element={
-                                        <div>
-                                            {error ? (
-                                                <p className="centered">
-                                                    Something went wrong
-                                                </p>
-                                            ) : (
-                                                <div>
-                                                    {isLoading ? (
-                                                        <p className="loading-text">
-                                                            Loading...
-                                                        </p>
-                                                    ) : (
-                                                        <div>
-                                                            {characters.length <
-                                                                1 &&
-                                                            hasNotSearchedOnce ===
-                                                                false ? (
-                                                                <p className="centered">
-                                                                    No results
-                                                                    found,
-                                                                    please
-                                                                    refresh
-                                                                </p>
-                                                            ) : (
-                                                                <CharacterGrid
-                                                                    value={
-                                                                        searchCharacter
-                                                                    }
-                                                                    searchCharacter={
-                                                                        searchCharacter
-                                                                    }
-                                                                    characters={
-                                                                        characters
-                                                                    }
-                                                                    onChange={
-                                                                        this
-                                                                            .onSearchChange
-                                                                    }
-                                                                    onSubmit={
-                                                                        this
-                                                                            .onSearchSubmit
-                                                                    }
-                                                                />
-                                                            )}
-                                                        </div>
-                                                    )}
-                                                </div>
-                                            )}
-                                        </div>
-                                    }
-                                />
-                                <Route path="/" element={<HomePage />} />
-                            </Routes>
+                            <button
+                                type="button"
+                                onClick={toggleDarkTheme}
+                                className="dark-button ui red medium button search-button"
+                            >
+                                Dark Mode
+                            </button>
                         </div>
-                        <Footer />
-                    </Container>
-                </div>
+                        <Container>
+                            <h1 className="page-header">Marvel for Noobs</h1>
+                        </Container>
+                        <Menu.Item className="menu-item header-link nav-button">
+                            <Link to="/">Home</Link>
+                        </Menu.Item>
+                        <Menu.Item className="menu-item header-link nav-button">
+                            <Link to="/characters">Characters</Link>
+                        </Menu.Item>
+                    </Menu>
+                    <div>
+                        <Routes>
+                            <Route
+                                path="characters/*"
+                                element={
+                                    <div>
+                                        {error ? (
+                                            <p className="centered">
+                                                Something went wrong
+                                            </p>
+                                        ) : (
+                                            <div>
+                                                {isLoading ? (
+                                                    <p className="loading-text">
+                                                        Loading...
+                                                    </p>
+                                                ) : (
+                                                    <div>
+                                                        {characters.length <
+                                                            1 &&
+                                                        hasNotSearchedOnce ===
+                                                            false ? (
+                                                            <p className="centered">
+                                                                No results
+                                                                found, please
+                                                                refresh
+                                                            </p>
+                                                        ) : (
+                                                            <CharacterGrid
+                                                                value={
+                                                                    searchCharacter
+                                                                }
+                                                                searchCharacter={
+                                                                    searchCharacter
+                                                                }
+                                                                characters={
+                                                                    characters
+                                                                }
+                                                                onChange={
+                                                                    onSearchChange
+                                                                }
+                                                                onSubmit={
+                                                                    onSearchSubmit
+                                                                }
+                                                            />
+                                                        )}
+                                                    </div>
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                }
+                            />
+                            <Route path="/" element={<HomePage />} />
+                        </Routes>
+                    </div>
+                </Container>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Main;
