94e33a830878d7772d0078e8a3f8bfa9fae94648
Hadi Ahmad <hadiahmad676@gmail.com>
2022-09-06 00:51:42 +0000

    Edited countdown component to reflect Hadi's new implementation

--- src/components/CountdownText/index.js
+++ src/components/CountdownText/index.js
@@ -2,17 +2,32 @@ import React from "react";
 import Countdown from "react-countdown";
 
 const Complete = () => <span>In theaters now!</span>;
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+        return <Complete />;
+    } else {
+        return (
+            <span>
+                {days}D {hours}H {minutes}M {seconds}S{" "}
+            </span>
+        );
+    }
+};
 
 /**
  * Displays a countdown for movies
  */
 const CountdownText = ({ timetill, name }) => (
     <div>
-        <h3 className="countdown-name">{name}</h3>
-        <div className="countdown-wrapper countdowntext-time">
-            <Countdown date={timetill}>
-                <Complete />
-            </Countdown>
+        <div className="card-header">
+            <h3 className="countdown-name">{name}</h3>
+        </div>
+        <div className="card-body">
+            <div className="countdown-wrapper countdowntext-time">
+                <Countdown renderer={renderer} date={timetill}>
+                    <Complete />
+                </Countdown>
+            </div>
         </div>
     </div>
 );
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,40 @@
 import React from "react";
 import CountdownText from "../CountdownText";
 
-const timeTillThor = "08 Jul 2022 00:00:00 GMT";
-const timeTillBlackPanther = "11 Nov 2022 00:00:00 GMT";
-const timeTillAntMan = "17 Feb 2023 00:00:00 GMT";
-const timeTillGuardians = "05 May 2023 00:00:00 GMT";
-const timeTillMarvels = "28 Jul 2023 00:00:00 GMT";
+const movies = [
+    {
+        name: "Ant-Man and the Wasp: Quantumania",
+        timeTill: "17 Feb 2023 00:00:00 GMT",
+    },
+    {
+        name: "Guardians Of The Galaxy Vol. 3",
+        timeTill: "05 May 2023 00:00:00 GMT",
+    },
+    {
+        name: "Thor: Love and Thunder",
+        timeTill: "08 Jul 2022 00:00:00 GMT",
+    },
+    {
+        name: "Black Panther 2",
+        timeTill: "11 Nov 2022 00:00:00 GMT",
+    },
+    {
+        name: "The Marvels",
+        timeTill: "28 Jul 2023 00:00:00 GMT",
+    },
+    {
+        name: "Blade",
+        timeTill: "03 Nov 2023 00:00:00 GMT",
+    },
+    {
+        name: "Captain America: New World Order",
+        timeTill: "03 May 2024 00:00:00 GMT",
+    },
+    {
+        name: "Thunderbolts",
+        timeTill: "26 Jul 2024 00:00:00 GMT",
+    },
+];
 
 /**
  * Responsible for home page tab
@@ -15,48 +44,20 @@ const HomePage = () => (
         <div>
             <h1 className="homepage-heading">Upcomimg Marvel Movies</h1>
             <br />
-            <div>
-                <div>
-                    <CountdownText
-                        name={"Ant-Man And The Wasp: Quantumania"}
-                        timetill={timeTillAntMan}
-                    />
-                </div>
-                <br />
-                <br />
-                <div>
-                    <CountdownText
-                        name={"Guardians Of The Galaxy Vol. 3"}
-                        timetill={timeTillGuardians}
-                    />
-                </div>
-                <br />
-                <br />
-                <div>
-                    <CountdownText
-                        name={"Thor: Love and Thunder"}
-                        timetill={timeTillThor}
-                    />
-                </div>
-                <br />
-                <br />
-                <div>
-                    <CountdownText
-                        name={"Black Panther 2"}
-                        timetill={timeTillBlackPanther}
-                    />
-                </div>
-                <br />
-                <br />
-                <div>
-                    <CountdownText
-                        name={"The Marvels"}
-                        timetill={timeTillMarvels}
-                    />
-                </div>
+            <div className="countdowns">
+                {movies.map((movie, key) => {
+                    return (
+                        <div className="card" key={key}>
+                            <CountdownText
+                                name={movie.name}
+                                timetill={movie.timeTill}
+                            />
+                            <br />
+                            <br />
+                        </div>
+                    );
+                })}
             </div>
-            <br />
-            <br />
             <h4 style={{ textAlign: "center" }}>More coming soon</h4>
         </div>
     </div>
