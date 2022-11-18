114f50a226bacf434c22ffa140bcfcfa5f55578f
Madiba Hudson-Quansah <mhquansah@gmail.com>
2022-11-18 13:54:54 +0000

    (fix): removed dead wiki link

--- src/components/CharacterSingle/index.js
+++ src/components/CharacterSingle/index.js
@@ -28,50 +28,68 @@ const CharacterSingle = ({
         </Link>
         <h2 className="character-name">{name}</h2>
         <p className="description">
-            {description === ""
-                ? "No description listed for this character/group in the Marvel API. Please click the wiki link below the events section to find out more at the official marvel website"
-                : description}
+            {description === "" || description === " " ? (
+                <a target="_blank" rel="noopener noreferrer" href={urls[0].url}>
+                    No description listed for this character/group in the Marvel
+                    API. Please click to find out more at the official marvel
+                    website
+                </a>
+            ) : (
+                description
+            )}
         </p>
         <br />
         <div className="extras">
             <h2>Comics</h2>
-            {comics.items.map((comic, key) => (
-                <div key={key}>
-                    <h3>
-                        {comic.available === 0
-                            ? "No comics listed for this character"
-                            : comic.name}
-                    </h3>
-                </div>
-            ))}
+            {comics.available === 0 ? (
+                <h3> Comics have not been listed for this character</h3>
+            ) : (
+                comics.items.map((comic, key) => (
+                    <div key={key}>
+                        <h3>
+                            {comic.available === 0
+                                ? "No comics listed for this character"
+                                : comic.name}
+                        </h3>
+                    </div>
+                ))
+            )}
             <br />
             <h2>Events</h2>
-            {events.items.map((characterEvent, key) => (
-                <div key={key}>
-                    <h3>
-                        {characterEvent.available === 0
-                            ? `${name} events have not been listed`
-                            : characterEvent.name}
-                    </h3>
-                </div>
-            ))}
+            {events.available === 0 ? (
+                <h3> Events have not been listed for this character</h3>
+            ) : (
+                events.items.map((characterEvent, key) => (
+                    <div key={key}>
+                        <h3>
+                            {events.available === 0
+                                ? `${name} events have not been listed`
+                                : characterEvent.name}
+                        </h3>
+                    </div>
+                ))
+            )}
             <br />
             <br />
             <div>
-                {urls.map((link, key) => (
-                    <div key={key}>
-                        <br />
-                        <h3 style={{ marginTop: "5px" }}>{link.type}</h3>
-                        <a
-                            key={key}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href={link.url}
-                        >
-                            Read More
-                        </a>
-                    </div>
-                ))}
+                {urls
+                    .filter((link) => link.type !== "wiki")
+                    .map((link, key) => (
+                        <div key={key}>
+                            <br />
+                            <a
+                                key={key}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href={link.url}
+                            >
+                                <h3 style={{ marginTop: "5px" }}>
+                                    {link.type.charAt(0).toUpperCase() +
+                                        link.type.slice(1)}
+                                </h3>
+                            </a>
+                        </div>
+                    ))}
             </div>
         </div>
     </div>
