a2006fd7aa260222248217784665b7c02e68938b
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-06-06 19:08:17 +0000

    fix(build): Alternate entry point to prevent page not found errors

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     return {
-        build: {},
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, "index.html"),
+                    e200: resolve(__dirname, "200.html"),
+                },
+            },
+        },
         plugins: [react()],
     };
 });
